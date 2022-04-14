ecc0a8aa9c5b1cefa38ed9e12ba2d69c56f30da0
Ellpeck <me@ellpeck.de>
2022-04-14 15:20:26 +0200

    initial setup

---
+++ src/main/java/de/ellpeck/voodoodolls/VoodooDolls.java
@@ -0,0 +1,10 @@
+package de.ellpeck.voodoodolls;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VoodooDolls.ID)
+public class VoodooDolls {
+
+    public static final String ID = "voodoodolls";
+
+}
